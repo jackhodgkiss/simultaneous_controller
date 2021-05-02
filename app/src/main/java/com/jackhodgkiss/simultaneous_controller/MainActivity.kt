4d54f434312ead906a6f746856bc85718e6daa00
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2021-05-02 19:17:29 +0100

    Application can scan for nearby BLE devices
    The application is now capable of scanning for nearby BLE devices, any
    device found will have their MAC address printed to logcat.

--- app/src/main/java/com/jackhodgkiss/simultaneous_controller/MainActivity.kt
+++ app/src/main/java/com/jackhodgkiss/simultaneous_controller/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.jackhodgkiss.simultaneous_controller
 
 import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,8 +19,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun scanForDevices(context: Context) {
-        context.runWithPermissions(Manifest.permission.ACCESS_COARSE_LOCATION) {
-            Log.d("Permissions", "Permission to access coarse location granted")
+        val adapter = BluetoothAdapter.getDefaultAdapter()
+
+        context.runWithPermissions(Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_ADMIN) {
+            adapter.bluetoothLeScanner.startScan(callback)
         }
     }
+
+    private val callback = object : ScanCallback() {
+        override fun onBatchScanResults(results: MutableList<ScanResult>?) {
+            results?.forEach { result -> deviceFound(result.device) }
+        }
+
+        override fun onScanResult(callbackType: Int, result: ScanResult?) {
+            result?.let { deviceFound(result.device) }
+        }
+    }
+
+    private fun deviceFound(device: BluetoothDevice) {
+        Log.d("BLE", device.address)
+    }
+
 }
