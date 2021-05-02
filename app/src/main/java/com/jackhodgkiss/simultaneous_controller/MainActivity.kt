03997b014c88c842162063cae7b9414c578c6cbd
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2021-05-02 17:12:46 +0100

    QuickPermissions-Kotlin asks for permission
    The QuickPermissions-Kotlin asks the user for permission to access
    coarse location with appropriate dialog window before continuing to
    execute the remainder of the function. This is as expected.

--- app/src/main/java/com/jackhodgkiss/simultaneous_controller/MainActivity.kt
+++ app/src/main/java/com/jackhodgkiss/simultaneous_controller/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.jackhodgkiss.simultaneous_controller
 
+import android.Manifest
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.livinglifetechway.quickpermissions_kotlin.runWithPermissions
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        scanForDevices(this)
+    }
+
+    fun scanForDevices(context: Context) {
+        context.runWithPermissions(Manifest.permission.ACCESS_COARSE_LOCATION) {
+            Log.d("Permissions", "Permission to access coarse location granted")
+        }
     }
 }
