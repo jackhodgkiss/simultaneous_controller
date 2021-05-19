ffac5f39e4d62e7e4b623a16d79afae62164b3ac
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2021-05-19 18:35:40 +0100

    Alert dialog for edit text added

--- app/src/main/java/com/jackhodgkiss/simultaneous_controller/SensorAdapter.kt
+++ app/src/main/java/com/jackhodgkiss/simultaneous_controller/SensorAdapter.kt
@@ -1,8 +1,12 @@
 package com.jackhodgkiss.simultaneous_controller
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
+import android.text.InputType
 import android.util.Log
 import android.view.*
+import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
 import com.jackhodgkiss.simultaneous_controller.databinding.SensorItemBinding
 
@@ -32,12 +36,14 @@ class SensorAdapter(private val sensors: ArrayList<SensorItem>) :
             R.string.preference_file_key.toString(),
             Context.MODE_PRIVATE
         )
+        private lateinit var sensor: SensorItem
 
         init {
             itemBinding.root.setOnCreateContextMenuListener(this)
         }
 
         fun bindSensor(sensor: SensorItem) {
+            this.sensor = sensor
             itemBinding.sensorNameTextView.text = sensor.name
             itemBinding.sensorAddressTextView.text = sensor.address
             itemBinding.sensorRssiTextView.text =
@@ -78,12 +84,32 @@ class SensorAdapter(private val sensors: ArrayList<SensorItem>) :
         private val clickListener: MenuItem.OnMenuItemClickListener = object : MenuItem.OnMenuItemClickListener {
             override fun onMenuItemClick(item: MenuItem?): Boolean {
                 when(item?.itemId) {
-                    0 -> Log.d("Menu", "Edit Me $itemId")
+                    0 -> editSensorName()
                     1 -> Log.d("Menu", "Identify $itemId")
                     else -> return false
                 }
                 return true
             }
         }
+
+        private fun editSensorName() {
+            val builder = AlertDialog.Builder(itemBinding.root.context)
+            val input = EditText(itemBinding.root.context)
+            builder.setTitle("Edit Name")
+            builder.setView(input)
+            builder.setPositiveButton("OK", object : DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    sensor.name = input.text.toString()
+                    itemBinding.sensorNameTextView.text = sensor.name
+                }
+            })
+            builder.setNegativeButton("Cancel", object : DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    dialog?.cancel()
+                }
+            })
+            builder.show()
+        }
+
     }
 }
--- app/src/main/java/com/jackhodgkiss/simultaneous_controller/SensorItem.kt
+++ app/src/main/java/com/jackhodgkiss/simultaneous_controller/SensorItem.kt
@@ -1,5 +1,5 @@
 package com.jackhodgkiss.simultaneous_controller
 
-class SensorItem(val name: String, val address: String, val rssi_values: ArrayList<Int>, var is_favourite: Boolean) {
+class SensorItem(var name: String, val address: String, val rssi_values: ArrayList<Int>, var is_favourite: Boolean) {
     
 }
