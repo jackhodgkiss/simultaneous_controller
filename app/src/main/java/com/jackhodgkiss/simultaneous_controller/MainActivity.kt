1aaa05a20401c344fe7e94b0c3347512bf0a8cc0
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2021-05-26 17:01:00 +0100

    Navigation bar swaps between fragments

--- app/src/main/java/com/jackhodgkiss/simultaneous_controller/MainActivity.kt
+++ app/src/main/java/com/jackhodgkiss/simultaneous_controller/MainActivity.kt
@@ -2,10 +2,37 @@ package com.jackhodgkiss.simultaneous_controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.jackhodgkiss.simultaneous_controller.databinding.ActivityMainBinding
+import com.jackhodgkiss.simultaneous_controller.fragments.ExperimentPlannerFragment
+import com.jackhodgkiss.simultaneous_controller.fragments.ScannerFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val scannerFragment = ScannerFragment()
+        val experimentPlannerFragment = ExperimentPlannerFragment()
+
+        setCurrentFragment(scannerFragment)
+
+        binding.navigationBar.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.ic_baseline_bluetooth_searching -> setCurrentFragment(scannerFragment)
+                R.id.ic_baseline_science -> setCurrentFragment(experimentPlannerFragment)
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame_wrapper, fragment)
+            commit()
+        }
     }
 }
---
+++ app/src/main/java/com/jackhodgkiss/simultaneous_controller/fragments/ExperimentPlannerFragment.kt
@@ -0,0 +1,22 @@
+package com.jackhodgkiss.simultaneous_controller.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jackhodgkiss.simultaneous_controller.R
+
+class ExperimentPlannerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_experiment_planner, container, false)
+
+    }
+
+}
