8e7c06f1c22ea8a7a60cd7b843df05c539f073a8
Jack Hodgkiss <52131498+jackhodgkiss@users.noreply.github.com>
2021-06-01 17:22:52 +0100

    Add initial experiment planner ui

--- app/src/main/java/com/jackhodgkiss/simultaneous_controller/fragments/ExperimentPlannerFragment.kt
+++ app/src/main/java/com/jackhodgkiss/simultaneous_controller/fragments/ExperimentPlannerFragment.kt
@@ -6,17 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.jackhodgkiss.simultaneous_controller.R
+import com.jackhodgkiss.simultaneous_controller.databinding.FragmentExperimentPlannerBinding
+import com.jackhodgkiss.simultaneous_controller.databinding.FragmentScannerBinding
 
 class ExperimentPlannerFragment : Fragment() {
+    private lateinit var binding: FragmentExperimentPlannerBinding
+    private lateinit var gesture_spinner: Spinner
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_experiment_planner, container, false)
+    ): View {
+        binding = FragmentExperimentPlannerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val gestureSpinner = binding.gestureSpinner
+        ArrayAdapter.createFromResource(
+            view.context,
+            R.array.gestures_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            gestureSpinner.adapter = adapter
+        }
     }
 
 }
